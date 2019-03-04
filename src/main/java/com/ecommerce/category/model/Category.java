840c6dd6933105f42e89105f76b9c5c5595ea208
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2019-03-04 19:08:17 +0700

    initial upload

---
+++ src/main/java/com/ecommerce/category/model/Category.java
@@ -0,0 +1,26 @@
+package com.ecommerce.category.model;
+
+public class Category {
+
+    private String id;
+    private String name;
+
+    public Category() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
